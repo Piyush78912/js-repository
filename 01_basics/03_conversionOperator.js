6d47076b5d92bf5b773dc0c83fbb145958d5161f
Piyush Khatri <135313304+Piyush78912@users.noreply.github.com>
2024-12-18 12:46:07 +0000

    Conversion of Data Types

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,30 @@
+let score = "Piyush"
+//console.log(typeof(score))
+let valueInNumber = Number(score); //string
+// console.log(typeof(valueInNumber)) //number 
+// console.log(valueInNumber) //Nan (Not a Number)
+
+
+// "33" => 33 can be convert easily 
+// "33abc" => NaN
+//true => 1; false = 0
+
+let isLoggedIn = "Piyush";
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)  //true
+
+
+//1 => true  0 =>false
+// "" =>false
+// "piyush" => true  
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);  //3 
+console.log(typeof(stringNumber));  //3 
+
+
+
