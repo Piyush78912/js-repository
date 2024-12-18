59f3914abb9681fe97698ce384cfb4455ddd7b3e
Piyush Khatri <135313304+Piyush78912@users.noreply.github.com>
2024-12-18 09:53:34 +0000

    datatypes config done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict";  //treats all js code as newer version 
 console.log('Piyush') ;
 
 console.log('Hitesh')
@@ -6,6 +6,10 @@ console.log('Hitesh')
 let name = "abc"
 let age = 21
 let isLogged = true;
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3+3) //code readability should be high
 
 // // number = 2^53
 // bigInt
