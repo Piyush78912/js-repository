9386de59cbb30e3866c0cb7b82c8dd628f9f78c5
Piyush Khatri <135313304+Piyush78912@users.noreply.github.com>
2024-12-18 09:49:18 +0000

    datatypes config done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -7,7 +7,13 @@ let name = "abc"
 let age = 21
 let isLogged = true;
 
-
+// // number = 2^53
+// bigInt
+// string = " "
+// boolean = true/false;
+// null = standalone value
+// undefined = undefined value 
+// symbol = unique 
 console.log(typeof name);
 console.log(typeof null);
 console.log(typeof undefined);
