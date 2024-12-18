66ad7be448218968ddc50c10ca6145ad9027cc00
Piyush Khatri <135313304+Piyush78912@users.noreply.github.com>
2024-12-18 09:46:53 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+console.log('Piyush') ;
+
+console.log('Hitesh')
+
+let name = "abc"
+let age = 21
+let isLogged = true;
+
+
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
+
